21c704fb545183d3b5b74ec74af887b14a01f2a1
spymethod <spymethod254@gmail.com>
2026-09-03 02:45:11 +0300

    Create pages/Boosting.jsx

---
+++ src/pages/Boosting.jsx
@@ -0,0 +1,9 @@
+export default function Boosting() {
+  return (
+    <div style={{padding:20}}>
+      <h1>🚀 Social Boost</h1>
+      <p>Boost your socials!</p>
+      <button style={{width:'100%', padding:12, background:'black', color:'white'}}>Buy 100 Instagram Followers - KES 200</button>
+    </div>
+  )
+}

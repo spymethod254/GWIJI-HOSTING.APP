36a1965a9ee0443caad4fa32ef6bb3ae71b4fbfc
spymethod <spymethod254@gmail.com>
2026-09-03 02:46:13 +0300

    Create pages/Earning.jsx

---
+++ src/pages/Earning.jsx
@@ -0,0 +1,14 @@
+export default function Earning() {
+  return (
+    <div style={{padding:20}}>
+      <h1>💰 Earn with Gwiji</h1>
+      <p>Complete tasks and earn KES!</p>
+      <div style={{border:'1px solid #ddd', padding:15, marginTop:10}}>
+        <b>Task 1: Follow Instagram</b> - KES 10 <button style={{marginLeft:10}}>Do Task</button>
+      </div>
+      <div style={{border:'1px solid #ddd', padding:15, marginTop:10}}>
+        <b>Task 2: Subscribe YouTube</b> - KES 20 <button style={{marginLeft:10}}>Do Task</button>
+      </div>
+    </div>
+  )
+}
